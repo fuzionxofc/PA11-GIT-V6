d96c842e9d344a8af46e1fef1261a1b41f249ecc
fuzionxofc <51284928+fuzionxofc@users.noreply.github.com>
2020-06-18 14:48:36 +0800

    Updated by Bryan

---
+++ 21_PA11_Bryan/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager thismanager;
+    private int scores;
+    public Text txt_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thismanager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateScore(int score)
+    {
+        scores += score;
+        txt_score.text = "Score: " + scores;
+    }
+}
---
+++ 21_PA11_Bryan/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalmove = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(0f, verticalmove, 0f);
+
+        Vector3 clampPos = transform.position;
+        clampPos.y = Mathf.Clamp(clampPos.y, -3.89f, 3.88f);
+        transform.position = clampPos;
+
+        //transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
